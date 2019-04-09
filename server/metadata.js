98cb24c3d95f8213186747cb4dfad9f00910e65f
jjjulllesss <43120077+jjjulllesss@users.noreply.github.com>
2019-04-09 23:22:34 +0200

    essai meta
    Affichage des données éssentielles
    Ne focntionne pas pour le moment
    Obliger de passer par fichier externe

--- server/metadata.js
+++ server/metadata.js
@@ -6,7 +6,12 @@ var contents = fs.readFileSync("../common/profiles/parameter.json");
 var jsonContent = JSON.parse(contents);
 
 ffmpeg.ffprobe(`../uploads/${jsonContent.nomvid}`, function(err, metadata) {
-    console.dir(metadata);
+
+    fs.writeFileSync('../common/metadata.json', JSON.stringify(metadata))
+    var contentsMeta = fs.readFileSync("../common/metadata.json");
+    var jsonMeta = JSON.parse(contentsMeta);
+    let codec = `${jsonMeta.streams.codec_name}`;
+    console.log(codec);
 });
 
 //regler les parametres de sortie
