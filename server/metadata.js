fb2f2d8e06cae451ca8ff2ea2a80c4bf61ea1d14
jjjulllesss <43120077+jjjulllesss@users.noreply.github.com>
2019-04-09 11:28:07 +0200

    lm

--- server/metadata.js
+++ server/metadata.js
@@ -5,6 +5,6 @@ const path = require('path');
 var contents = fs.readFileSync("../common/profiles/parameter.json");
 var jsonContent = JSON.parse(contents);
 
-ffmpeg.ffprobe(`../uploads/${jsonContent.nomvid} -show_entries stream=avg_frame_rate,height,width,channels`, function(err, metadata) {
+ffmpeg.ffprobe(`../uploads/${jsonContent.nomvid}`, function(err, metadata) {
     console.dir(metadata);
 });
