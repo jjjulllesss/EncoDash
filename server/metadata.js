8fe47514f895d14ccbec37df34c48577c0bd4870
jjjulllesss <43120077+jjjulllesss@users.noreply.github.com>
2019-04-10 21:57:20 +0200

    metadata
    OK

--- server/metadata.js
+++ server/metadata.js
@@ -9,9 +9,14 @@ ffmpeg.ffprobe(`../uploads/${jsonContent.nomvid}`, function(err, metadata) {
 
     fs.writeFileSync('../common/metadata.json', JSON.stringify(metadata))
     var contentsMeta = fs.readFileSync("../common/metadata.json");
-    var jsonMeta = JSON.parse(contentsMeta);
-    let codec = `${jsonMeta.streams.codec_name}`;
-    console.log(codec);
+    var obj = JSON.parse(contentsMeta);
+
+    var width = obj.streams[0].width;
+    var height = obj.streams[0].height;
+    var r_frame_rate = obj.streams[0].r_frame_rate;
+    var nb_streams = obj.format.nb_streams;
+
+    console.log(width, height, r_frame_rate, nb_streams);
 });
 
 //regler les parametres de sortie
